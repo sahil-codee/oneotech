8b08d02048e2004b3b5485ba78ed2d2c27f95e90
sahil <sahilbakshi05@gmail.com>
2024-07-20 00:29:39 +0530

    Avoid large layout shifts

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -6,17 +6,21 @@ import { css } from "styled-components/macro"; //eslint-disable-line
 import Header from "components/headers/light.js";
 import Footer from "components/footers/MiniCenteredFooter.js";
 import MainFeature1 from "components/features/TwoColWithButton.js";
-// import MainFeature2 from "components/features/TwoColSingleFeatureWithStats.js";
-// import MainFeature3 from "components/features/TwoColSingleFeatureWithStats2.js";
-import Features from "components/features/ThreeColSimple.js";
-// import Features from "components/features/ThreeColWithSideImage.js";
-// import TeamCardGrid from "components/cards/ProfileThreeColGrid.js";
-
-import SupportIconImage from "images/support-icon.svg";
-import ShieldIconImage from "images/shield-icon.svg";
-import CustomerLoveIconImage from "images/simple-icon.svg";
+import aboutImage from "../images/hero-screenshot-2.webp";
 
 const Subheading = tw.span`uppercase tracking-wider text-sm`;
+export const MissionSubheading = tw.h5`font-bold text-primary-500`;
+
+const FullWidthDescription = styled.div`
+  ${tw`w-full text-center py-8`}
+`;
+
+const Description = tw.p`lg:text-lg font-medium leading-relaxed text-secondary-100`;
+
+const DescriptionWithSpacing = styled(Description)`
+  ${tw`mb-8`}// Add bottom margin for spacing
+`;
+
 export default () => {
   return (
     <AnimationRevealPage>
@@ -25,22 +29,49 @@ export default () => {
       <MainFeature1
         subheading={<Subheading>About OneOTech</Subheading>}
         heading="Welcome to OneOTech Enterprises"
-        description="At OneOTech Enterprises, we specialize in providing top-quality industrial and consumer products to meet the diverse needs of our global clientele. With years of experience in the trading industry, we have built a reputation for reliability, excellence, and customer satisfaction."
+        description={
+          <Description>
+            As a leading exporter of high-quality products, our company takes
+            great pride in offering a diverse range of goods to customers around
+            the world. From the rich, aromatic teas of Assam and Darjeeling to
+            the vibrant, flavorful spices of India, our expertly curated
+            selection caters to a wide array of tastes and culinary needs.
+            Beyond just tea and spices, we also excel in the export of essential
+            industrial supplies like cable ties, DIN rails, and cable tie mounts
+            - durable, versatile products relied upon by businesses and
+            industries globally.
+          </Description>
+        }
         buttonRounded={false}
         primaryButtonText="See Products"
         primaryButtonUrl="/products"
-        imageSrc="https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=768&q=80"
+        imageSrc={aboutImage}
       />
-      <MainFeature1
-        subheading={<Subheading>Our Mission</Subheading>}
-        heading="Bridging Quality and Customer Needs."
-        buttonRounded={false}
-        primaryButtonText="Contact Us"
-        description="Our mission is to bridge the gap between quality products and our customers' needs, ensuring a seamless and efficient trading experience. We are committed to delivering exceptional value through our comprehensive range of products and unmatched service."
-        imageSrc="https://images.unsplash.com/3/doctype-hi-res.jpg?ixlib=rb-1.2.1&auto=format&fit=crop&w=768&q=80"
-        textOnLeft={false}
-      />
-      <Features
+      <FullWidthDescription>
+        <MissionSubheading>Our Mission</MissionSubheading>
+        <DescriptionWithSpacing>
+          Our commitment to excellence extends far beyond our product range. As
+          a trusted spice exporter in India, we are proud to serve clients in
+          key markets such as the UAE, Dubai, USA, UK, London, Australia,
+          Canada, New Zealand, and Germany. Our reputation as a premier spice
+          exporter in India, as well as a leading provider of nylon cable ties,
+          cable tie mounts, and DIN rails, is a testament to the care and
+          precision we bring to every transaction. With a keen focus on
+          delivering the highest standards of quality and customer service, we
+          have established ourselves as a go-to source for businesses and
+          consumers seeking the finest Indian exports.
+        </DescriptionWithSpacing>
+        <Description>
+          Whether you're in need of fragrant Darjeeling teas, aromatic whole and
+          powdered spices, or essential industrial hardware, our team of experts
+          is dedicated to exceeding your expectations. We stand ready to connect
+          you with the products you require, tailoring our offerings to meet the
+          unique needs of markets around the world. Experience the difference
+          that our unparalleled expertise and commitment to excellence can make
+          in your business or personal endeavors.
+        </Description>
+      </FullWidthDescription>
+      {/* <Features
         subheading={<Subheading>Our Vision</Subheading>}
         heading="Aiming for Global Leadership."
         description="We aim to be a global leader in trading, recognized for our commitment to quality and innovation."
@@ -71,7 +102,7 @@ export default () => {
           },
         ]}
         linkText=""
-      />
+      /> */}
       {/* <TeamCardGrid subheading={<Subheading>Our Team</Subheading>} /> */}
       <Footer />
     </AnimationRevealPage>
