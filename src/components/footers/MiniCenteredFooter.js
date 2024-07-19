08e628b9f09467b64f886943fcce8530ef0e0b99
sahil <sahilbakshi05@gmail.com>
2024-07-19 16:26:36 +0530

    Avoid large layout shifts

--- src/components/footers/MiniCenteredFooter.js
+++ src/components/footers/MiniCenteredFooter.js
@@ -13,13 +13,13 @@ const Content = tw.div`max-w-screen-xl mx-auto py-20 lg:py-24`;
 const Row = tw.div`flex items-center justify-center flex-col px-8`;
 
 const LogoContainer = tw.div`flex items-center justify-center md:justify-start`;
-const LogoImg = tw.img`w-8`;
+const LogoImg = tw.img`w-8 h-auto`; // Added height auto to maintain aspect ratio
 const LogoText = tw.h5`ml-2 text-2xl font-black tracking-wider`;
 
 const LinksContainer = tw.div`mt-8 font-medium flex flex-wrap justify-center items-center flex-col sm:flex-row`;
 const Link = tw.a`border-b-2 border-transparent hocus:text-gray-300 hocus:border-gray-300 pb-1 transition duration-300 mt-2 mx-4`;
 
-const SocialLinksContainer = tw.div`mt-10`;
+const SocialLinksContainer = tw.div`mt-10 flex justify-center`; // Added flex to align icons horizontally
 const SocialLink = styled.a`
   ${tw`cursor-pointer inline-block text-gray-100 hover:text-gray-500 transition duration-300 mx-4`}
   svg {
@@ -28,37 +28,45 @@ const SocialLink = styled.a`
 `;
 
 const CopyrightText = tw.p`text-center mt-10 font-medium tracking-wide text-sm text-gray-600`;
+
+// Adding a footer-specific CSS class to ensure consistent height
+const FooterWrapper = styled.div`
+  ${tw`min-h-[100px]`}/* Set a minimum height to avoid layout shifts */
+`;
+
 export default () => {
   return (
     <Container>
       <Content>
-        <Row>
-          <LogoContainer>
-            <LogoImg src={logo} alt={"oneotech enterpises logo"} />
-            <LogoText>Oneotech Enterprises</LogoText>
-          </LogoContainer>
-          <LinksContainer>
-            <Link href="/">Home</Link>
-            <Link href="/about">About</Link>
-            <Link href="/contact">Contact Us</Link>
-            <Link href="/products">Products</Link>
-            <Link href="/services">Services</Link>
-          </LinksContainer>
-          <SocialLinksContainer>
-            <SocialLink href="https://facebook.com">
-              <FacebookIcon />
-            </SocialLink>
-            <SocialLink href="https://twitter.com">
-              <TwitterIcon />
-            </SocialLink>
-            <SocialLink href="https://youtube.com">
-              <YoutubeIcon />
-            </SocialLink>
-          </SocialLinksContainer>
-          <CopyrightText>
-            &copy; Copyright 2024, Oneotech Enterprises All Rights Reserved.
-          </CopyrightText>
-        </Row>
+        <FooterWrapper>
+          <Row>
+            <LogoContainer>
+              <LogoImg src={logo} alt="Oneotech Enterprises logo" />
+              <LogoText>Oneotech Enterprises</LogoText>
+            </LogoContainer>
+            <LinksContainer>
+              <Link href="/">Home</Link>
+              <Link href="/about">About</Link>
+              <Link href="/contact">Contact Us</Link>
+              <Link href="/products">Products</Link>
+              <Link href="/services">Services</Link>
+            </LinksContainer>
+            <SocialLinksContainer>
+              <SocialLink href="https://facebook.com">
+                <FacebookIcon />
+              </SocialLink>
+              <SocialLink href="https://twitter.com">
+                <TwitterIcon />
+              </SocialLink>
+              <SocialLink href="https://youtube.com">
+                <YoutubeIcon />
+              </SocialLink>
+            </SocialLinksContainer>
+            <CopyrightText>
+              &copy; Copyright 2024, Oneotech Enterprises All Rights Reserved.
+            </CopyrightText>
+          </Row>
+        </FooterWrapper>
       </Content>
     </Container>
   );
